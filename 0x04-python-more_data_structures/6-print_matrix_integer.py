40d250815ebb104bff85fec4d8380888f3c635b5
george-ashraf-88 <georgeashraf88@icloud.com>
2024-01-27 06:08:25 -0800

    lists and tuples

--- 0x04-python-more_data_structures/6-print_matrix_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        count = 0
-        for x in row:
-            count += 1
-            print('{:d}'.format(x), end=(" " if count < len(row) else ""))
-        print()
