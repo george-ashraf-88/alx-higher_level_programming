37cf641456557184cfcfe06733c65197a5aa925a
george-ashraf-88 <georgeashraf88@icloud.com>
2024-02-22 01:16:11 -0800

    Mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,5 @@
+Project Name
+0x06. Python - Classes and Objects
+
+Author 
+   GEORGE ASHRAF 😎😎
